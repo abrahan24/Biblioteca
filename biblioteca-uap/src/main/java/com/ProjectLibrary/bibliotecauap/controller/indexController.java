4da9e984a1e006fdcf37549f9453fa7fc2ebc721
MARGARINOWEY <cridichinaus998@gmail.com>
2022-09-02 14:28:27 -0400

    index modificado

--- biblioteca-uap/src/main/java/com/ProjectLibrary/bibliotecauap/controller/indexController.java
+++ biblioteca-uap/src/main/java/com/ProjectLibrary/bibliotecauap/controller/indexController.java
@@ -1,36 +1,88 @@
 package com.ProjectLibrary.bibliotecauap.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.ProjectLibrary.bibliotecauap.models.service.IApartadoService;
+import com.ProjectLibrary.bibliotecauap.models.entity.Libro;
+import com.ProjectLibrary.bibliotecauap.models.service.ILibroService;
 
 @Controller
 public class indexController {
-    
-    @RequestMapping(value  = "/index",method = RequestMethod.GET) // Pagina principal
-	public String principal(){
+
+	@Autowired
+	private ILibroService libroService;
+	
+	@RequestMapping(value = "/index", method = RequestMethod.GET) // Pagina principal
+	public String principal() {
 		
-		
+
 		return "Principal";
 	}
-	//========= Pagina de Registro Persona=====
+	// ========= Pagina de Registro Persona=====
 
-	@RequestMapping(value  = "/registroP",method = RequestMethod.GET) // Pagina principal
-	public String RegistroPersona(){
-		
-		
+	@RequestMapping(value = "/registroP", method = RequestMethod.GET) // Pagina principal
+	public String RegistroPersona() {
+
 		return "RegistroPersona";
 	}
 
-	@RequestMapping(value  = "/registroL",method = RequestMethod.GET) // Pagina principal
-	public String RegistroLibro(){
+	//==========================================Libro============================================================================
+	// ========= Pagina de libro=====
+	@RequestMapping(value = "/Libro", method = RequestMethod.GET) // Pagina principal
+	public String Libro(@Validated Libro libro,Model model) {
 		
-		
-		return "RegistroLibro";
+		model.addAttribute("libro", new Libro()); //capturar los datos de los inputs (1)		
+
+		return "libro/RegistroLibro";
 	}
+	// ========= funcion para guardar registro de libro=====
+	@RequestMapping(value = "/registrarLibro", method = RequestMethod.POST) // Enviar datos de Registro a Lista
+	public String registrarLibro(@Validated Libro libro) { //validar los datos capturados (1)
+		
+		libroService.save(libro);
 
+		return "redirect:Libro";
+	}
+	// ========= Pagina de Lista de libro=====
+	@RequestMapping(value = "/listaL", method = RequestMethod.GET) // Pagina principal
+	public String ListaLibro(Model model) {
+		
+		model.addAttribute("libro", libroService.findAll()); //capturar los datos de los inputs (1) y mostrarlos en la tabla del HTML
 
-
+		return "Libro/ListaLibro";
+	}
+	// ========= funcion para eliminar registro de libro=====
+	@RequestMapping(value = "/eliminar-libro/{id_libro}")
+	public String EliminarLibro(@PathVariable("id_libro")Long id_libro){
+						
+		
+		Libro libro = libroService.findOne(id_libro);
+		
+		libro.setEstado("X");
+		
+		libroService.save(libro);
+		
+		return "redirect:/listaL";
+		
+	}
+	// ========= funcion para editar registro de libro=====
+	@RequestMapping(value = "/editar-libro/{id_libro}")
+	public String EditarLibro(@PathVariable("id_libro")Long id_libro, Model model){
+		
+		//personaService.delete(id_persona);
+		
+		
+		Libro libro = libroService.findOne(id_libro);
+		
+		model.addAttribute("libro", libro);		
+		
+		return "libro/RegistroLibro";
+		
+	}
+	//==========================================/Libro============================================================================
 }
